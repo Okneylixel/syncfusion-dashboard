3aa7e79d99bfdfb308f313d5ebf6afda95aeec00
okneylixel <okneylixel@gmail.com>
2022-10-14 20:17:20 +0300

    add context API state  navbar

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ import {
    Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban,
    Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line
 } from './pages'
+
+import { useStateContext } from "./context/ContextProvider"
+
 import './App.css'
 
+
 const App = () => {
-   const activeMenu = true
+   const { activeMenu } = useStateContext()
+
    return (
       <div>
          <BrowserRouter>
